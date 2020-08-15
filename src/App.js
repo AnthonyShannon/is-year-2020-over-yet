14d8478f2e74f9ab66ba90e6e29cb24d51917d2c
ashan <ashannon1990@gmail.com>
2020-08-15 14:48:12 -0400

    added styling and logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import IsItOver from './components/IsItOverAnswer'
-import './App.css';
+
+const styles = {
+  text: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontSize: 80
+  }
+}
 
 function App() {
+
+  console.log("We all know 2020 has been a really really bad year for many.")
+  console.log("It will eventually all be over, but at the time of creating this site it is still very much 2020.")
+  console.log("Everyone that lived through this year can confirm it was one of the worst years by far, but there is far too much that happened to list it here in the console.")
+  console.log("However, the year 2020 will be over eventually. Hopefully by then, we will be back to normal.")
+  console.log("anthonyshannon.dev")
 
   let thisYear = new Date().getFullYear()
 
   return (
-    <div className="App">
+    <div style={styles.text} className="App">
       <IsItOver
         answer={
           thisYear === 2020 ?
