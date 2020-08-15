af54db2faddd4b8b62c1bd8012ee3d0cb5277fba
ashan <ashannon1990@gmail.com>
2020-08-15 17:39:38 -0400

    added server side logic

---
+++ client/build/precache-manifest.2874546d1bc47d45ab2986054b3dde15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7166d2a9f1503eddf409378ce41fc321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18039b73be29ddcdf96",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5afe08f5b4d57dcc77f5",
+    "url": "/static/js/2.65af9782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65af9782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18039b73be29ddcdf96",
+    "url": "/static/js/main.6dab265d.chunk.js"
+  },
+  {
+    "revision": "8403d59b3524920c19ec",
+    "url": "/static/js/runtime-main.7cbf45cc.js"
+  }
+]);
