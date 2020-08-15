768e996d261361b122e22f55fd76834f07d71155
ashan <ashannon1990@gmail.com>
2020-08-15 14:07:34 -0400

    added logic to determine year

---
+++ src/components/IsItOverAnswer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IsItOver = (props) => {
+    return (
+        <div>
+            <h1>{props.answer}</h1>
+        </div>
+    );
+};
+
+export default IsItOver;
