d2b6f0362a56d924494e3089b3f5c148ad829ce0
ashan <ashannon1990@gmail.com>
2020-08-15 21:56:21 -0400

    express added

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
